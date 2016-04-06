209dc9e0635e731c1fd81bfec91095c0a768c09b
Alex Meuer <alexmeuer@gmail.com>
2016-04-07 00:35:52 +0100

    Commented and organized code.

--- MonitorLab/Game.cpp
+++ MonitorLab/Game.cpp
@@ -6,8 +6,10 @@ messageBuffer_(10),
 messageSpacing_(0.f, 30.f)
 {
 	//window_.setActive(false);
+
+	//Create the player.
 	assert(playerTex_.loadFromFile("player.png"));
-	player_ = new Player(playerTex_, {300.f, 300.f}, 20.f);
+	player_ = new Player(playerTex_, {400.f, 400.f}, 20.f);
 }
 
 Game::~Game() {
@@ -16,14 +18,18 @@ Game::~Game() {
 
 int Game::run() {
 	running_ = true;
+
+	//Start update thread
 	updateThread_ = boost::thread(boost::bind(&Game::doUpdateLoop, this));
+
+	//Start render thread
 	renderThread_ = boost::thread(boost::bind(&Game::doRenderLoop, this));
 
-	eventThread_.join();
+	//Wait for threads to be finished
 	renderThread_.join();
 	updateThread_.join();
 
-	return 0;
+	return 0;	//EXIT_SUCCESS
 }
 
 void Game::handleEvent(sf::Event& e) {
@@ -35,6 +41,7 @@ void Game::handleEvent(sf::Event& e) {
 			break;
 
 		case sf::Event::KeyPressed:
+			//Move the player if a directional key is pressed and deposit a message into the boundedbuffer
 			if (e.key.code == sf::Keyboard::Right) {
 				messageBuffer_.deposit("Moving right...");
 				player_->moveRight();
@@ -69,11 +76,13 @@ void Game::handleEvent(sf::Event& e) {
 }
 
 void Game::doUpdateLoop() {
+	//setup fixed timestep
 	auto clock = std::chrono::steady_clock();
 	const auto interval = std::chrono::milliseconds(UPDATE_INTERVAL);
 	auto lastTickTime = clock.now() - interval;
 
 	while (running_) {
+		//update at a set interval
 		if (lastTickTime + interval <= clock.now()) {
 			lastTickTime = clock.now();
 
@@ -89,6 +98,7 @@ void Game::update() {
 
 	printf("Updating.\n");
 
+	//Print a phrase if enough updates have passed (alternate between phrases)
 	if (++updatesSinceBoredom >= boredomInterval) {
 		if (sayAltPhrase) {		
 			messageBuffer_.deposit("Much Bound. Such Buffer. Wow.");
@@ -98,17 +108,21 @@ void Game::update() {
 			messageBuffer_.deposit("Pacman grows tired of your shenanigans...");
 			sayAltPhrase = true;
 		}
+		//Reset counter
 		updatesSinceBoredom = 0u;
 	}
 }
 
 void Game::doRenderLoop() {
+	//Begine message fetching thread
 	visibleMsgMakerThread_ = boost::thread(boost::bind(&Game::startMessageFetching, this));
 
+	//Create SFML window on this thread (it is important that this thread creates the window because other pollEvent won't work)
 	createWindow();
 
 	//window_->setActive(true);
 
+	//Setup fixed timestep
 	auto clock = std::chrono::steady_clock();
 	const auto interval = std::chrono::milliseconds(DRAW_INTERVAL);
 	auto lastDrawTime = clock.now() - interval;
@@ -116,10 +130,12 @@ void Game::doRenderLoop() {
 	sf::Event e;
 
 	while (running_) {
+		//Poll and handle any events
 		if (window_->pollEvent(e)) {
 			handleEvent(e);
 		}
 
+		//Draw at a fixed interval
 		if (lastDrawTime + interval <= clock.now()) {
 			lastDrawTime = clock.now();
 
@@ -128,6 +144,7 @@ void Game::doRenderLoop() {
 		}
 	}
 
+	//Wait for message fetching thread to finish
 	visibleMsgMakerThread_.join();
 }
 
@@ -137,20 +154,31 @@ void Game::draw() {
 	printf("\t\tDrawing.\n");
 
 	window_->clear();
+
+	//Draw the player
 	window_->draw(*player_);
 	
+	//Draw each visible mesage to the screen
 	visibleMessageListMutex.lock();
 	for (auto itr = visibleMessages_.begin(); itr != visibleMessages_.end(); ++itr) {
+
+		//Reduce the text object's time to live
 		itr->second -= DRAW_INTERVAL;
+		
 		if (itr->second > 0.f) {
+
+			//Shrink the text towards end of lifetime
 			if (itr->second < 3000.f && itr->first.getCharacterSize() > 0.f) {
 				itr->first.setCharacterSize(itr->second / 100.f);
 			}
+
+			//Draw the text object
 			window_->draw(itr->first);
 		}
 	}
 	visibleMessageListMutex.unlock();
 
+	//Render window to the screen
 	window_->display();
 
 	//windowMutex_.unlock();
@@ -160,15 +188,18 @@ void Game::startMessageFetching() {
 	sf::Font font;
 	assert(font.loadFromFile("COOPBL.TTF"));
 
+	//Millisecons that each visible msg will live for
 	const float timeToLive = 8000.f;
 
 	while (running_) {
 		//Fetch a string from the buffer
 		auto msg = messageBuffer_.fetch();
 
+		//Turn the string into a visible message
 		auto txt = sf::Text(msg, font);
 
 		visibleMessageListMutex.lock();
+		//Add the visible message to the list and give it its time to live.
 		visibleMessages_.push_front(std::make_pair(txt, timeToLive));
 
 		auto itr = visibleMessages_.begin();
@@ -178,7 +209,8 @@ void Game::startMessageFetching() {
 			//move it down the screen (make space for the new msg)
 			itr->first.move(messageSpacing_);
 
-			if (itr->second <= 0.f || itr->first.getCharacterSize() <= 1.f) {
+			//Remove any old messages
+			if (itr->second <= 0.f) {
 				itr = visibleMessages_.erase(itr);
 			} else {
 				++itr;
--- MonitorLab/Game.h
+++ MonitorLab/Game.h
@@ -8,7 +8,10 @@
 #include "BoundedBuffer.h"
 #include "Player.h"
 
+//Millisecons between each update loop iteration
 #define UPDATE_INTERVAL 8
+
+//Milliseconds between each draw loop iteration
 #define DRAW_INTERVAL 16
 
 class Game {
@@ -18,7 +21,6 @@ public:
 
 	int run();
 private:
-	void doEventLoop();
 	void handleEvent(sf::Event &e);
 
 	void doUpdateLoop();
@@ -27,23 +29,30 @@ private:
 	void doRenderLoop();
 	void draw();
 
+	//Begins message fetching loop. Does not return until running_ is false.
 	void startMessageFetching();
 
+	//Set to false to close the game.
 	boost::atomic<bool> running_;
 
 	sf::RenderWindow *window_;
-	//boost::mutex windowMutex_;
 	void createWindow();
 
 	sf::Texture playerTex_;
 	Player *player_;
 
-	boost::thread updateThread_, renderThread_, eventThread_, visibleMsgMakerThread_;
+	boost::thread updateThread_, renderThread_, visibleMsgMakerThread_;
 
+	//Bounded buffer of strings. Threads can deposit and fetch safely.
 	BoundedBuffer<std::string> messageBuffer_;
 
+	//! List of SFML Text objects and their time to live.
 	std::list<std::pair<sf::Text, float>> visibleMessages_;
+
+	//! Mutex for accessing visible msg list.
 	boost::mutex visibleMessageListMutex;
+
+	//Change in position between one visible msg and the next.
 	const sf::Vector2f messageSpacing_;
 };
 #endif
